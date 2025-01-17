8ae3004b225e1978d9a8c0234e710f499bf1b7d0
Ehan <admin@admins-iMac.local>
2025-01-17 11:10:11 +0000

    Create Authrization

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
+const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const JWT = require("jsonwebtoken");
 
 //middleware
 app.use(express.json());
-
+app.use(cookieParser())
+const corsOptions = {
+    origin: "http://localhost:8081", // Replace with your allowed origin
+    optionsSuccessStatus: 200,
+    allowedHeaders: ["Content-Type", "Authorization"], // Include Authorization header
+    credentials: true 
+  };
+app.use(cors(corsOptions));
 app.use("/api", apiRouter);
 
 module.exports = app;
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const usersRouter = express.Router();
 
-const { getUsers } = require("../controller/get-controllers");
-const { registerUser, loginUser } = require("../controller/post-controllers");
+const { getUsers} = require("../controller/get-controllers");
+const { registerUser, loginUser,authotization } = require("../controller/post-controllers");
 
 usersRouter.route("/").get(getUsers);
+usersRouter.route("/userData").post(authotization);
 usersRouter.route("/register").post(registerUser);
 usersRouter.route("/login").post(loginUser);
 
